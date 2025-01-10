fc73183d2d3af2abb1443004430f70453f0d1f71
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-10 22:11:20 +0100

    Add leaflet vue3

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+const zoom: number = 2;
+const center: [number, number] = [47.41322, -1.219482];
+</script>
+
+<template>
+  <div style="height:600px; width:800px">
+    <LMap ref="map" v-model:zoom="zoom" :center="center" :useGlobalLeaflet="false">
+      <LTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+      ></LTileLayer>
+    </LMap>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
