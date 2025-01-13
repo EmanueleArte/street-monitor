532a25b1b3b14153dcd6997481037ba152988386
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2025-01-13 18:14:27 +0100

    Make init-db script import multiple collections

--- backend/db/import/init-db.sh
+++ backend/db/import/init-db.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# This script loads each file and creates a mongodb collection named using the file name.
+# The Json files must be named 'db_<collection-name>.json' (example: 'db_users.json' become collection 'users')
+
 DB_NAME="dbStreetMonitor"
-DB_COLLECTION="users"
 
 echo "Importing databases"
 
-for f in "docker-entrypoint-initdb.d/db/*.json"
+for file in /docker-entrypoint-initdb.d/db/*.json
 do
-    mongoimport --host localhost --db $DB_NAME --collection $DB_COLLECTION --file $f
+    collection=${file##*_}
+    collection=${collection%.*}
+    mongoimport --host localhost --db $DB_NAME --collection $collection --file $file
 done
 
 echo "Imported data in $DB_NAME"
