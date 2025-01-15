2c180998fe50fc8518e4598aeda7521ce6225d07
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2025-01-15 10:37:53 +0100

    Fix typo

--- backend/src/models/notificationTypeModel.ts
+++ backend/src/models/notificationTypeModel.ts
@@ -2,12 +2,12 @@ import mongoose, { Document, Schema, Model } from "mongoose"
 
 export interface INotificationType extends Document {
     name: String,
-    icon_path: String
+    icon_address: String
 }
 
 export const notificationTypeSchema: Schema<INotificationType> = new Schema<INotificationType>({
     name: {type: String, required: true},
-    icon_path: {type: String, required: false}
+    icon_address: {type: String, required: false}
 })
 
 const notificationTypeModel: Model<INotificationType> = mongoose.model<INotificationType>('NotificationType', notificationTypeSchema)
