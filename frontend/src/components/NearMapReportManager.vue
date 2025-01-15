a51fbdd1a3e9b5ddec9e56438b6eb6494cc34a8d
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-15 16:43:17 +0100

    Add map position near reports

--- frontend/src/components/NearMapReportManager.vue
+++ frontend/src/components/NearMapReportManager.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-// 500m lat = 0.00449
-// 500m lng = 0.00635
 import axios from "axios"
 import { onMounted, ref } from "vue"
-import ReportPin from '@/components/ReportPin.vue'
+import ReportPin from "@/components/ReportPin.vue"
+import type { IReport } from "@models/reportModel"
 
 const props = defineProps<{
   lat: number,
-  lng: number
+  lng: number,
+  radius: number
 }>()
 
-const reports = ref<never[]>([])
+const reports = ref<IReport[]>([])
 
 const getNearReports = async () => {
   try {
-    reports.value = (await axios.post("http://localhost:3000/reports", { "lat": props.lat, "lng": props.lng })).data
+    // 500m lat = 0.00449
+    // 500m lng = 0.00635
+    reports.value = (await axios.get(
+        `http://localhost:3000/reports/by-coordinates/${props.lat}&${props.lng}&${props.radius}`)
+    ).data
   } catch (e) {
     console.error(e)
   }
