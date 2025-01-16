a5a7b118eccba60dd42645f9cf0a21a2b00e986e
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-16 09:59:12 +0100

    Add creation, update and delete of report types

--- backend/src/controllers/reportTypeController.ts
+++ backend/src/controllers/reportTypeController.ts
@@ -26,3 +26,40 @@ export const getReportTypeByName = (req: Request, res: Response) => {
             res.status(500).send(err)
         })
 }
+
+export const createReportType = (req: Request, res: Response) => {
+    const reportType = new reportTypeModel(req.body)
+    reportType.save()
+        .then((doc: IReportType) => {
+            res.json(doc)
+        })
+        .catch((err: Error) => {
+            res.status(500).send(err)
+        })
+}
+
+export const updateReportType = (req: Request, res: Response) => {
+    reportTypeModel.findOneAndUpdate({ name: req.params.name }, req.body, { new: true })
+        .then((doc: IReportType | null) => {
+            if (!doc) {
+                return res.status(404).send('Report type not found')
+            }
+            res.json(doc)
+        })
+        .catch((err: Error) => {
+            res.status(500).send(err)
+        })
+}
+
+export const deleteReportType = (req: Request, res: Response) => {
+    reportTypeModel.findOneAndDelete({ name: req.params.name })
+        .then((doc: IReportType | null) => {
+            if (!doc) {
+                return res.status(404).send('Report type not found')
+            }
+            res.json(doc)
+        })
+        .catch((err: Error) => {
+            res.status(500).send(err)
+        })
+}
