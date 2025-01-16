fa6fea30e6691e564109b69fac0e46201ff73064
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2025-01-16 09:17:05 +0100

    Add basic login page structure

--- frontend/src/components/LoginContainer.vue
+++ frontend/src/components/LoginContainer.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FormInput from './inputs/FormInput.vue';
+</script>
 
 <template>
-    login
+    <span>StreetMonitor</span>
+    <h1>Your city needs you!</h1>
+    <p>Sign in to make a difference.</p>
+
+    <form @submit.prevent="">
+        <fieldset>
+            <legend>Account information</legend>
+
+            <FormInput
+                fieldName="username"
+                label="username / email"
+                placeholder="Insert username or email"
+            />
+
+            <FormInput 
+                fieldName="password"
+                label="password"
+                type="password"
+            />
+        </fieldset>
+
+        <input type="submit" value="Sign In">
+    </form>
 </template>
