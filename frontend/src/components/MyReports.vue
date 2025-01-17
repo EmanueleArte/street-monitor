36c865a5710bb42166f68d20afa64159c6705bc6
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-17 10:25:38 +0100

    Add basic style

--- frontend/src/components/MyReports.vue
+++ frontend/src/components/MyReports.vue
@@ -3,5 +3,16 @@
 </script>
 
 <template>
-    <button>My Reports</button>
+    <button class="my-reports-button">My Reports</button>
 </template>
+
+<style lang="scss">
+@use "../style/vars" as *;
+
+.my-reports-button {
+    position: absolute;
+    bottom: 2em;
+    left: 1em;
+    z-index: 500;
+}
+</style>
