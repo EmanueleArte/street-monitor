d9cb62b44a13e1b17c623239cee2dd7722dcbc14
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-18 18:00:08 +0100

    Add report cards and data retrieval

--- frontend/src/components/MyReportsDiv.vue
+++ frontend/src/components/MyReportsDiv.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
-import { defineProps } from "vue"
+import type { IReport } from "@models/reportModel";
+import axios from "axios";
+import { defineProps, onMounted, ref } from "vue"
+import ReportCard from "./ReportCard.vue";
+
+const myReports = ref<IReport[]>([])
 
 const props = defineProps({
   isVisible: { type: Boolean, required: true }
 })
+
+const listMyReports = async () => {
+  try {
+    const data = (await axios.get("http://localhost:3000/reports/by-user/mariorossi")).data //TODO cambiare user (mariorossi)
+    data.forEach((report: IReport) => {
+      console.log(report)
+    })
+    myReports.value = data
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+onMounted(listMyReports)
 </script>
 
 <template>
     <div v-show="isVisible" class="my-reports-div bg-blue-400">
-        My reports:
+        <p>My reports:</p>
+        <ReportCard v-for="report in myReports" :report="report" />
     </div>
 </template>
 
---
+++ frontend/src/components/ReportCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    report: { type: Object, required: true }
+})
+
+const reportTypeTextConverter = (t: string): string => {
+    return t.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+const datetimeConverter = (datetime: string): string => {
+    return datetime.split('T').join(' ').split('.')[0].replace('Z', '');
+}
+
+</script>
+
+<template>
+    <article class="report-card bg-gray-400">
+        <section class="w-4/12">
+            <img :src="report.picture" alt="report image" class="report-image" />
+        </section>
+        <section class="w-8/12 pl-3 text-start">
+            <h3>{{ reportTypeTextConverter(report.type) }}</h3>
+            <p class="text-xs">{{ datetimeConverter(report.open_datetime) }}</p>
+            <p class="text-xs">{{ report.description }}</p>    
+        </section>
+    </article>
+</template>
+
+<style lang="scss">
+@use "../style/vars" as *;
+
+.report-card {
+    display: flex;
+    align-items: center;
+    padding: 0.8rem;
+    margin: 0.5rem;
+    border: solid 2px white;
+    border-radius: 18px;
+    margin-bottom: 0.4rem;
+}
+
+</style>
