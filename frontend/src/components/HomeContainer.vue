ca126a233603528ec9ef7f16bd35526f6ca92b3f
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-20 13:38:17 +0100

    Remove unused imports

--- frontend/src/components/HomeContainer.vue
+++ frontend/src/components/HomeContainer.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import axios from "axios"
-import { onMounted, ref } from "vue"
+import { ref } from "vue"
 import Map from "@/components/Map.vue"
 import MyReportsButton from "@/components/MyReportsButton.vue"
 import MyReportsDiv from "@/components/MyReportsDiv.vue"
