b04d00aec9c0b38cc6f31d721f10d79a8cf7de2e
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-20 11:48:44 +0100

    Make my reports scrollable

--- frontend/src/components/MyReportsDiv.vue
+++ frontend/src/components/MyReportsDiv.vue
@@ -27,25 +27,47 @@ onMounted(listMyReports)
 
 <template>
     <div v-show="isVisible" class="my-reports-div bg-blue-400">
-        <p>My reports:</p>
-        <ReportCard v-for="report in myReports" :report="report" />
+        <p class="my-reports-title">My reports:</p>
+        <div class="my-reports-container">
+          <ReportCard v-for="report in myReports" :report="report" />
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 @use "../style/vars" as *;
 
-.my-reports-div {
-    position: absolute;
-    bottom: 0;
-    left: 0.4rem;
-    right: 0.4rem;
-    height: 60%;
-    z-index: $home_page_divs_z_index;
-    padding: 0.8rem;
-    border: solid 2px white;
-    border-bottom : none;
-    border-radius: 18px 18px 0 0;
+@mixin my-reports {
+  $font-size: 16px; //1rem
+  $line-height: 1.5;
+  $margin: 0rem;
+  $p-height: calc(#{$font-size * $line-height} + #{$margin * 2});
+
+  font-size: $font-size;
+  line-height: $line-height;
+  margin: $margin;
+  max-height: calc(100% - #{$p-height});
 }
 
+.my-reports-title {
+  @include my-reports;
+}
+
+.my-reports-div {
+  position: absolute;
+  bottom: 0;
+  left: 0.4rem;
+  right: 0.4rem;
+  height: 60%;
+  z-index: $home_page_divs_z_index;
+  padding: 0.8rem;
+  border: solid 2px white;
+  border-bottom : none;
+  border-radius: 18px 18px 0 0;
+}
+
+.my-reports-container {
+  @include my-reports;
+  overflow-y: auto;
+}
 </style>
