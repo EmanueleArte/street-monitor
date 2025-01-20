d9710ade7893e399ded45b46020b2bbbb99e96b5
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-20 15:08:17 +0100

    Fix tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,24 +2,25 @@
 export default {
   content: ["./src/**/*.{html,ts,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      "button-text": "#ffffff",
-      "light": "#ffffff",
-      "main": {
-        100: "#dbeafe",
-        500: "#3b82f6",
-        600: "#2563eb",
-        700: "#1d4ed8",
+    extend: {
+      colors: {
+        "button-text": "#ffffff",
+        "light": "#ffffff",
+        "main": {
+          100: "#dbeafe",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+        },
+        gray: {
+          500: "#6b7280",
+        }
       },
-      gray: {
-        500: "#6b7280",
+      borderRadius: {
+        "xl": "0.75rem",
+        "full": "9999px",
       }
     },
-    borderRadius: {
-      "xl": "0.75rem",
-      "full": "9999px",
-    }
   },
   plugins: [],
 }
