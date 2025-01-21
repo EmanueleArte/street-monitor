1bc7cfcf452684ddf70ac087e8f67c649ac54b64
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-21 20:24:24 +0100

    Optimized using scss

--- frontend/src/components/transitions/SlideFromBottom.vue
+++ frontend/src/components/transitions/SlideFromBottom.vue
@@ -9,15 +9,17 @@ const emit = defineEmits(["onCompleted"])
 </template>
 
 <style scoped lang="scss">
-.slide-bottom-enter-active, .slide-bottom-leave-active {
-  transition: transform 0.5s;
-}
+.slide-bottom {
+  &-enter-active, &-leave-active {
+    transition: transform 0.5s;
+  }
 
-.slide-bottom-enter-from {
-  transform: translateY(100%);
-}
+  &-enter-from {
+    transform: translateY(100%);
+  }
 
-.slide-bottom-leave-to {
-  transform: translateY(100%);
+  &-leave-to {
+    transform: translateY(100%);
+  }
 }
 </style>
