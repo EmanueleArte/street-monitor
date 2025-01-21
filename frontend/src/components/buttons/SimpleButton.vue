016be7d59b457998555a909ba84f02d46fa238dd
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-21 16:58:08 +0100

    Add simple button component

---
+++ frontend/src/components/buttons/SimpleButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  classes?: string
+}>()
+const emit = defineEmits(["action"])
+</script>
+
+<template>
+  <button :class="[classes, 'rounded-xl bg-main-600 text-button-text px-4 py-2 hover:bg-main-700 duration-300']"
+          @click="emit('action')">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+
+</style>
