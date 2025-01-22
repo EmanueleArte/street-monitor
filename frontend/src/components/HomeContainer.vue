6ce8f13500f08877932f332102b5708a42fc02c0
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2025-01-22 15:20:10 +0100

    Add navbar

--- frontend/src/components/HomeContainer.vue
+++ frontend/src/components/HomeContainer.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
 import Map from "@/components/Map.vue"
 import NewReport from "@/components/NewReportTile.vue"
 import MyReports from "@/components/MyReports.vue"
@@ -7,6 +8,7 @@ import MySpots from "@/components/MySpots.vue"
 
 <template>
   <div class="homeContainer w-screen h-screen">
+    <NavBar />
     <Map class="z-0" :zoom="12" :usePosition=true></Map>
     <NewReport/>
     <MyReports/>
