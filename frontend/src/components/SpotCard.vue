65d39d3c148636b2276363e0e6e287690c251ab0
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-23 16:45:30 +0100

    Show my spots

---
+++ frontend/src/components/SpotCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { IFavoriteSpot } from '@models/favoriteSpotModel';
+import type { PropType } from 'vue';
+
+defineProps({
+    spot: { type: Object as PropType<IFavoriteSpot>, required: true }
+})
+</script>
+
+<template>
+    <article class="w-[calc(50%-1rem)] h-fit p-1 m-2 rounded-lg bg-surface-component border-2 border-primary-700">
+        <p class="text-lg">{{ spot.label }}</p>
+        <p class="text-xs"> Lat: {{ spot.coordinates[0] }}</p>
+        <p class="text-xs"> Lon: {{ spot.coordinates[1] }}</p>
+    </article>
+</template>
