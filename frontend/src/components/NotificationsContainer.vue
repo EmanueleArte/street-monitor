d018ff7a502159f66800ed948e45fdd3904a7505
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2025-01-23 10:18:16 +0100

    Display dates from the latest to the oldest

--- frontend/src/components/NotificationsContainer.vue
+++ frontend/src/components/NotificationsContainer.vue
@@ -2,19 +2,19 @@
 import Notification from './notifications/Notification.vue'
 
 const dates: Date[] = [
-    new Date('01/23/2025,10:05:00'),
-    new Date('01/23/2025,9:00:00'),
-    new Date('01/23/2025,7:00:00'),
     new Date('01/22/2025,10:00:00'),
-    new Date('01/21/2025,23:00:00'),
+    new Date('01/23/2025,10:05:00'),
+    new Date('01/01/2025,10:00:00'),
+    new Date('01/23/2025,7:00:00'),
+    new Date('01/23/2025,9:00:00'),
     new Date('01/21/2025,10:00:00'),
-    new Date('01/01/2025,10:00:00')
+    new Date('01/21/2025,23:00:00'),
 ]
 </script>
 
 <template>
     <ul>
-        <li v-for="date in dates">
+        <li v-for="date in dates.sort((a: Date, b: Date) => b.getTime() - a.getTime())">
             <Notification :read="false" :date="date">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, quis?
             </Notification>
