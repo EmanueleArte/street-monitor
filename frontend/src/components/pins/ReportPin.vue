1ef6b28a1d664b8dc1d0fe64e0dfdcb404b4346d
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-26 15:38:41 +0100

    Add report pin icons

--- frontend/src/components/pins/ReportPin.vue
+++ frontend/src/components/pins/ReportPin.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import type { IReport } from "@models/reportModel.ts"
 import Pin from "@/components/pins/Pin.vue"
+import { computed } from "vue"
 
 const props = defineProps<{
   report: IReport
 }>()
 
 const iconSize: number = 48
+const iconPath = computed(() => "../src/assets/icons/" + props.report.type + "_pin.png")
 </script>
 
 <template>
-  <Pin :lat="props.report.coordinates[0]" :lng="props.report.coordinates[1]" iconUrl="../src/assets/icons/temp.png"
+  <Pin :lat="props.report.coordinates[0]" :lng="props.report.coordinates[1]" :iconUrl="iconPath"
        :iconSize="iconSize"/>
 </template>
 
