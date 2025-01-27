a4ca8aecfd505f4cdd42ceffaf456bcb20fcc744
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-27 12:16:33 +0100

    Add filter checkboxes

--- frontend/src/components/ReportFilter.vue
+++ frontend/src/components/ReportFilter.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import type { IReport } from "@models/reportModel.ts"
-import { onMounted, onUnmounted, ref } from "vue"
+import { onMounted, onUnmounted, reactive, ref, watch } from "vue"
 import SlideFromTop from "@/components/transitions/SlideFromTop.vue"
-
-const props = defineProps<{
-  reports?: IReport[]
-}>()
+import Checkbox from "@/components/inputs/Checkbox.vue"
+import { ReportStatus } from "@/lib/vars.ts"
+import { formatUnderscoredString } from "@/lib/stringUtility.ts"
 
 const show = ref<boolean>(true)
 const observer = ref<MutationObserver | null>(null)
@@ -23,7 +22,36 @@ const observeTile = () => {
   observer.value.observe(document.body, { childList: true, subtree: true })
 }
 
-onMounted(observeTile)
+const statusToShow = reactive<Record<ReportStatus, boolean>>(
+    Object.values(ReportStatus).reduce((acc, status) => {
+      acc[status] = true
+      return acc
+    }, {} as Record<ReportStatus, boolean>)
+)
+statusToShow[ReportStatus.CLOSED] = false
+watch(statusToShow, () => {
+  console.log(statusToShow)
+})
+
+const typesToShow = {}
+const getReportTypes = () => {
+}
+// const getReportTypes = () => {
+//   axios.get<string[]>("http://localhost:3000/reports/types")
+//       .then((res) => {
+//         res.data.forEach((type) => {
+//           typesToShow[type] = true
+//         })
+//       })
+//       .catch((e) => {
+//         console.error(e)
+//       })
+// }
+
+onMounted(() => {
+  observeTile()
+  getReportTypes()
+})
 onUnmounted(() => {
   observer.value?.disconnect()
 })
@@ -33,7 +61,12 @@ onUnmounted(() => {
   <SlideFromTop>
     <div v-if="show" class="absolute w-screen top-10 p-4">
       <div class="w-full bg-surface-default shadow-lg rounded-xl p-4">
-        filter
+        <section class="flex flex-row justify-around">
+          <Checkbox v-for="(value, key) in statusToShow" :checked="value"
+                    @click="statusToShow[key] = !statusToShow[key]">
+            {{ formatUnderscoredString(key) }}
+          </Checkbox>
+        </section>
       </div>
     </div>
   </SlideFromTop>
---
+++ frontend/src/components/inputs/Checkbox.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  classes?: string,
+  checked?: boolean
+}>()
+</script>
+
+<template>
+  <div class="flex items-center">
+    <input :checked="checked" type="checkbox" value="" :class="[classes, 'w-4 h-4 text-primary-600 bg-surface-default border-gray-300 rounded-xl duration-300']">
+    <label class="ms-2 text-sm">
+      <slot></slot>
+    </label>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
