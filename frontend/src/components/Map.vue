df4c387120cddd19eac7cfc3166ea5cba39f34e6
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-01-29 15:57:32 +0100

    Add center to position button

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import "leaflet/dist/leaflet.css"
-import { LMap, LMarker, LTileLayer, LCircle } from "@vue-leaflet/vue-leaflet"
+import { LMap, LMarker, LTileLayer, LCircle, LControl } from "@vue-leaflet/vue-leaflet"
 import type { LeafletEvent } from "leaflet"
 import { ref, onUnmounted, onBeforeMount, watch } from "vue"
 import { throttle } from "lodash"
@@ -9,6 +9,7 @@ import CenterPin from "@/components/pins/CenterPin.vue"
 import { usePositionStore } from "@/stores/position.store.ts"
 import { coordsEquals } from "@/lib/mapUtility.ts"
 import { useReportStore } from "@/stores/report.store"
+import FloatingRoundButton from "@/components/buttons/FloatingRoundButton.vue"
 
 const reportStore = useReportStore()
 
@@ -47,6 +48,11 @@ const moveToPosition = (pos: [number, number]) => {
         animate: true,
         duration: 1
       })
+      if (usePositionStore().flyMainMap) {
+        usePositionStore().setFlyMainMap(false)
+        usePositionStore().positionToMove = [0, 0]
+        centerToPosition.value = true
+      }
     }
   }
 }
@@ -104,19 +110,37 @@ function unsetReport() {
   reportStore.setReport(undefined)
 }
 
+const recenterToPosition = () => {
+  usePositionStore().setFlyMainMap(true)
+  usePositionStore().positionToMove = usePositionStore().position
+}
+
 onBeforeMount(startWatchingPosition)
 onUnmounted(stopWatchingPosition)
 </script>
 
 <template>
   <LMap ref="map" :zoom="zoom" :center="center" :useGlobalLeaflet="false"
-        :options="{ zoomControl: false, attributionControl: false }" @ready="onMapReady" @drag="onMapMoved" @click="unsetReport">
+        :options="{ zoomControl: false, attributionControl: false }" @ready="onMapReady" @drag="onMapMoved"
+        @click="unsetReport">
     <LTileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
     ></LTileLayer>
-    <LMarker :lat-lng="usePositionStore().position" :options="{ alt: 'Current position' }" />
+    <LControl position="bottomleft">
+      <FloatingRoundButton class="fixed top-52 z-50" @click="recenterToPosition"
+                           screenReaderLabel="Center to current position">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5"
+             class="size-8 stroke-light">
+          <path d="M12 19.5a7.5 7.5 0 1 0 0-15 7.5 7.5 0 0 0 0 15"
+                stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6m0-11V2M4 12H2m10 8v2m8-10h2"
+                stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </FloatingRoundButton>
+    </LControl>
+    <LMarker :lat-lng="usePositionStore().position" :options="{ alt: 'Current position' }"/>
     <CenterPin v-if="!coordsEquals(center, usePositionStore().position)" :center="center"/>
     <LCircle
         :lat-lng="[center[0], center[1]]"
