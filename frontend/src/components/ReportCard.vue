86ba2050f0e7414b92204900c9393a9fac6e57c9
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-01-30 15:33:55 +0100

    Add moving to report when clicking on coords

--- frontend/src/components/ReportCard.vue
+++ frontend/src/components/ReportCard.vue
@@ -11,9 +11,11 @@ import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
 import PopoverPanelWrapper from './utils/PopoverPanelWrapper.vue';
 import SimpleButton from './buttons/SimpleButton.vue';
 import axios from 'axios';
+import { usePositionStore } from '@/stores/position.store';
 
 const emit = defineEmits(["updateTiles"])
 const authStore = useAuthStore()
+const positionStore = usePositionStore()
 
 const props = defineProps({
     report: { type: Object as PropType<IReport>, required: true }
@@ -43,6 +45,11 @@ const changeStatus = async () => {
         // send notification
         console.log(`sending notification to ${props.report.user}...`)
     }
+}
+
+const moveToReport = () => {
+    positionStore.setFlyMainMap(true)
+    positionStore.move(props.report.coordinates)
 }
 
 const reportTypeTextConverter = (t: string): string => {
@@ -97,9 +104,9 @@ const reputationColor = ref<string>(computeReputationColor(userReputation.value)
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-5 my-auto">
                             <path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" />
                         </svg>
-                        <a class="cursor-pointer text-primary-600 my-auto">
+                        <p @click="moveToReport" class="cursor-pointer text-primary-600 my-auto">
                             {{ coordinatesConverter(props.report.coordinates) }}
-                        </a>
+                        </p>
                     </div>
                     <!-- <ChangeStatusButton v-if="report.status!='closed'" :report="report" @changeStatus="changeStatus" /> -->
                     <SimpleButton
