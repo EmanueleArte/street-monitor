a369ce5ca0feb135a80bd7df4621aa0669852df5
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-02-03 18:02:42 +0100

    Fix click outside navbar

--- frontend/src/components/Aside.vue
+++ frontend/src/components/Aside.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <aside class="absolute w-96 h-3/5 bg-surface-default top-16 right-4 shadow-lg overflow-auto rounded-lg">
+    <aside class="nav-aside absolute w-96 h-3/5 bg-surface-default top-16 right-4 shadow-lg overflow-auto rounded-lg">
         <slot />
     </aside>
 </template>
