08d9ba686caa420bc9d48e68d0f0a3d7bbe2e8a1
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-02-03 14:44:07 +0100

    Put carousel desktop arrows to left and right

--- frontend/src/components/ReportCarosel.vue
+++ frontend/src/components/ReportCarosel.vue
@@ -63,32 +63,33 @@ const updateCarosel = (report: IReport) => {
 <template>
     <section
         v-if="currentReport"
-        class="z-10 absolute bottom-2 left-0 flex w-full md:content-center justify-center md:justify-start overflow-x-hidden md:w-96 md:gap-1 md:rounded-none md:border-0 md:left-1/4">
-        
-        <ReportCard
-            v-if="previousReport"
-            :report="previousReport"
-            :previousOrNext="true"
-            @click="previousReportHandler"
-            class="scale-95 shrink-0 md:hidden" />
+        class="z-10 absolute bottom-2 left-0 flex w-full md:content-center justify-center md:justify-start
+        overflow-x-hidden md:w-fit md:gap-1 md:rounded-none md:border-0 md:left-1/4">
 
-        <ReportCard :report="currentReport" class="shrink-0 basis-4/5 md:basis-auto md:w-4/5" />
-
-        <ReportCard
-            v-if="nextReport"
-            :report="nextReport"
-            :previousOrNext="true"
-            @click="nextReportHandler"
-            class="scale-95 shrink-0 md:hidden" />
-
-        <div class="flex-col justify-center gap-y-2 hidden md:flex">
-            <NavButton screen-reader-label="previous report" @click="previousReportHandler">
+        <div class="flex flex-row justify-center items-center gap-x-2 md:flex">
+            <NavButton screen-reader-label="previous report" @click="previousReportHandler" class="hidden md:block">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-7 m-auto">
                     <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
                 </svg>
             </NavButton>
+
+          <ReportCard
+              v-if="previousReport"
+              :report="previousReport"
+              :previousOrNext="true"
+              @click="previousReportHandler"
+              class="scale-95 shrink-0 md:hidden" />
+
+          <ReportCard :report="currentReport" class="shrink-0 basis-4/5 md:basis-auto md:w-80" />
+
+          <ReportCard
+              v-if="nextReport"
+              :report="nextReport"
+              :previousOrNext="true"
+              @click="nextReportHandler"
+              class="scale-95 shrink-0 md:hidden" />
             
-            <NavButton screen-reader-label="next report" @click="nextReportHandler">
+            <NavButton screen-reader-label="next report" @click="nextReportHandler" class="hidden md:block">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-7 m-auto">
                     <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                 </svg>
