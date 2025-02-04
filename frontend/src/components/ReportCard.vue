05016899064f2c374343f35f56a3b356ca65264b
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2025-02-04 16:38:01 +0100

    Fix upvote

--- frontend/src/components/ReportCard.vue
+++ frontend/src/components/ReportCard.vue
@@ -54,11 +54,15 @@ const changeStatus = async () => {
 
 const upvote = async () => {
     try {
-        props.report.upvotes?.push(authStore.get()?.username)
-        await axios.put(`http://localhost:3000/reports/by-id/${props.report._id}`, props.report)
-        if (user.value) {
-            user.value.reputation += 1
-            await axios.put(`http://localhost:3000/users/${props.report.user}`, user.value)
+        const response = await axios.get(`http://localhost:3000/users/${props.report.user}`)
+        if (response.status === 200) {
+            user.value = response.data
+            props.report.upvotes?.push(authStore.get()?.username)
+            await axios.put(`http://localhost:3000/reports/by-id/${props.report._id}`, props.report)
+            if (user.value) {
+                user.value.reputation += 1
+                await axios.put(`http://localhost:3000/users/${props.report.user}`, user.value)
+            }
         }
     } catch (e) {
         console.error(e)
