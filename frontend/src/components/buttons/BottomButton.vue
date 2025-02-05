9a8c3579edc8e78f427f543ab3a8be663a659231
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-02-05 15:33:50 +0100

    Refactor buttons

--- frontend/src/components/buttons/BottomButton.vue
+++ frontend/src/components/buttons/BottomButton.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 
 defineProps({
-    text: { type: String, required: true }
+  text: { type: String, required: true }
 })
 
 </script>
 
 <template>
-    <button class="absolute bottom-2 shadow-lg h-20 w-32 z-1 bg-primary-600 text-xl text-light rounded-xl">{{ text }}</button>
+  <button class="absolute bottom-2 shadow-lg h-20 w-32 z-1 bg-primary-600 text-xl text-light rounded-xl">
+    {{ text }}
+  </button>
 </template>
