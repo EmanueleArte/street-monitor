bf6a9c6698ceb669289049ce71aa2256905cfdc1
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-02-05 15:49:25 +0100

    Refactor lib

--- frontend/src/lib/passwordManager.ts
+++ frontend/src/lib/passwordManager.ts
@@ -1,7 +1,7 @@
 import { genSaltSync, hashSync, compareSync, hash, compare } from "bcrypt-ts"
 
 const SALT_VALUE: number = 10
-const salt: string = genSaltSync(SALT_VALUE);
+const salt: string = genSaltSync(SALT_VALUE)
 
 export default function hashPassword(password: string, sync: boolean = false): Promise<string> {
     return sync ? Promise.resolve(hashSync(password, salt)) : hash(password, salt)
