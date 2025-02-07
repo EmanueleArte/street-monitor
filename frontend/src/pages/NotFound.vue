94f42ddebbd982c19cf3806c46c704436b4203ba
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2025-02-07 16:15:53 +0100

    Add 'Page not found' page

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <img src="../assets/img/404.jpg" alt="Page not found"/>
-  </div>
+  <section class="m-auto flex w-fit flex-col gap-1 rounded px-10 py-20 text-center md:gap-2 md:px-30">
+    <h1 class="text-3xl font-bold text-primary-600">Oops! You Took a Wrong Turn</h1>
+    <div class="text-9xl font-semibold text-primary-600 drop-shadow-lg">404</div>
+    <h2 class="mt-4 text-base text-black/80">Looks Like a Missing Sign Led You Here.</h2>
+    <a class="mt-10 flex w-fit cursor-pointer gap-x-2 hover:text-primary-600" href="/">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+      </svg>
+      Back to safety
+    </a>
+  </section>
 </template>
